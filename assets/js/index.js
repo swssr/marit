8abae13e99cf4c11cb769c50105e1b45a26db6b6
swssr <simocmo4@gmail.com>
2019-07-28 22:09:07 +0200

    Modal works pefectly

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,3 +15,24 @@ function toggleNav([entry]) {
 const navIO = new IntersectionObserver(toggleNav, navOption);
 
 navIO.observe(header);
+
+//Modal toggler
+const learnLinks = document.querySelectorAll("[data-trigger-for]");
+const modalTexts = document.querySelectorAll("[data-modal-text-for]");
+//
+const modal = document.querySelector(".modal");
+const modalHead = document.querySelector(".modal__head");
+const modalBody = document.querySelector(".modal__text");
+const modalClose = document.querySelector("#btnModalClose");
+
+learnLinks.forEach((link, index) => {
+  link.addEventListener("click", e => {
+    const modHead = link.parentNode.querySelector(".card__head").textContent;
+
+    modalHead.textContent = modHead;
+    modalBody.textContent = modalTexts[index].textContent;
+    modal.showModal();
+  });
+});
+
+modalClose.addEventListener("click", () => modal.close());
